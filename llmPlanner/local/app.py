452e8c56056ec3936526f8903c1296f6dd4e8313
metriiics <metriics@yandex.ru>
2026-04-21 19:55:39 +1000

    fix local prog

--- llmPlanner/local/app.py
+++ llmPlanner/local/app.py
@@ -9,6 +9,12 @@ from db import ini_db
 class GenerateRequest(BaseModel):
     quest: str
 
+class GenerateResponse(BaseModel):
+    resp: str
+
+class HealthCheckResponse(BaseModel):
+    status: str
+
 app = FastAPI(
     title="Local LLM Planner",
     version="0.0.1",
@@ -27,16 +33,18 @@ async def startupEvent():
     """ Инициализация при запуске программы """
     ini_db()
 
-@app.get("/health")
+@app.get("/health", response_model=HealthCheckResponse)
 async def healthCheck():
     """ Проверка состояния API """
 
-    return "Ok!"
+    return HealthCheckResponse(status="Ok!")
 
-@app.post("/generate")
+@app.post("/generate", response_model=GenerateResponse)
 async def generate(request: GenerateRequest):
+    """ Возвращает ответ модели """
+
     response = agent.invoke({"messages": [("user", request.quest)]})
-    return response["messages"][-1].content
+    return GenerateResponse(resp=response["messages"][-1].content)
 
 if __name__ == "__main__":
     uvicorn.run(
